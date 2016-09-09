594a7ab4126814cd606ddc5d0b174532afa7c718
aliccce <aliceg@outlook.com>
2016-09-09 16:58:06 +0200

    Does the stuff on color images instead of greyscale

--- oving1/oppg3.py
+++ oving1/oppg3.py
@@ -49,6 +49,25 @@ def convolution(image, kernel):
     return new_image
 
 
+def convolve_three_channels(r, g, b, kernel):
+    new_r = convolution(r, kernel)
+    print("... Convolved red channel")
+    new_g = convolution(g, kernel)
+    print("... Convolved green channel")
+    new_b = convolution(b, kernel)
+    print("... Convolved blue channel")
+    return new_r, new_g, new_b
+
+
+def rearrange_channels(r, g, b, image):
+    print("... Rearranging")
+    image[..., 0] = r
+    image[..., 1] = g
+    image[..., 2] = b
+    return image
+
+
+
 """ Create kernels """
 box_blur = [[1/9]*3]*3
 
@@ -64,24 +83,38 @@ for i in range(len(gaussian)):
 
 """ Read file """
 
-image = misc.imread(".\Cute-a-bunny.jpg", flatten=True)
+image = misc.imread("./Cute-a-bunny.jpg")
 
-""" Do convolution """
+r = image[..., 0]
+g = image[..., 1]
+b = image[..., 2]
 
-box_blur_image = convolution(image, box_blur)
-gaussian_image = convolution(image, gaussian)
+""" Do convolution with box blur """
+
+print("Convolving with box blur")
+box_blur_image_r, box_blur_image_g, box_blur_image_b = convolve_three_channels(r, g, b, box_blur)
+box_blur_image = np.zeros_like(image)
+box_blur_image = rearrange_channels(box_blur_image_r, box_blur_image_g, box_blur_image_b, box_blur_image)
+
+""" Do convolution with gaussian """
+
+print("Convolving with gaussian")
+gaussian_r, gaussian_g, gaussian_b = convolve_three_channels(r, g, b, gaussian)
+gaussian_image = np.zeros_like(image)
+gaussian_image = rearrange_channels(gaussian_r, gaussian_g, gaussian_b, gaussian_image)
 
 """ Plot and save results """
 
+print("Plotting")
 _, ax = plt.subplots(1, 3)
-ax[0].imshow(image, interpolation="nearest", cmap=plt.cm.gray)
-ax[1].imshow(gaussian_image, interpolation="nearest", cmap=plt.cm.gray)
-ax[2].imshow(box_blur_image, interpolation="nearest", cmap=plt.cm.gray)
+ax[0].imshow(image, interpolation="nearest", vmax=255, vmin=0)
+ax[1].imshow(gaussian_image, interpolation="nearest", vmax=255, vmin=0)
+ax[2].imshow(box_blur_image, interpolation="nearest", vmax=255, vmin=0)
 ax[0].set_axis_off()
 ax[1].set_axis_off()
 ax[2].set_axis_off()
 
-
+print("Saving")
 misc.imsave('oppg3-before.png', image)
 misc.imsave('oppg3-box_blur.png', gaussian_image)
 misc.imsave('oppg3-gaussian.png', box_blur_image)
