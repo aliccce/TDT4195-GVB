dcb50a11e98b0d9791fa8e1bd229a9307ae4617a
Maren Orvedal <marenor@stud.ntnu.no>
2016-10-03 15:14:08 +0200

    Added oving3 oppg 1a, 1b and resulting images. Note: those are not saved upon running the script

---
+++ oving3/1a.py
@@ -0,0 +1,60 @@
+import numpy as np
+from scipy import misc
+import matplotlib.pyplot as plt
+import math
+
+def read_img(path):
+	image = misc.imread(path)
+	return image
+
+def threshold_alg(img, t_init, dT): #t_init = initial threshold, dT = delta T
+	t_previous = 0
+	t = t_init
+	s1 = [] #set of pixels where X > T
+	s2 = [] #set of pixels where X <= T
+	a = img.shape[0]
+	b = img.shape[1]
+	m1 = 0 #Mean of s1
+	m2 = 0 #Mean of s2
+
+	while (math.fabs(t - t_previous) > dT):
+		for x in range(a):
+			for y in range(b):
+				if (img[x, y] > t):
+					s1.append(img[x,y])
+				else:
+					s2.append(img[x,y])
+		temp1 = 0
+		for element in s1:
+			temp1 += element
+		m1 = temp1 / len(s1)
+
+		temp2 = 0
+		for element in s2:
+			temp2 += element
+		m2 = temp2 / len(s2)
+
+		t_previous = t
+		t = (m1 + m2)/2
+
+	return t
+
+def avg_gsi(img): #greyscale intensity
+	a = img.shape[0]
+	b = img.shape[1]
+
+	avg = 0	
+
+	for x in range(a):
+		for y in range(b):
+			avg += img[x, y]	
+			
+	return avg / (a*b)
+
+image = read_img('images/Fig1051(a)(defective_weld).tif')
+image = image.astype(np.float32)
+
+print "Average greyscale intensity in image: ", avg_gsi(image)
+t_init = 173
+dT = 0.5
+print "Determined threshold with values t_init =", t_init, ", dT =", dT, ":", threshold_alg(image, t_init, dT)
