88de06c20882d6f7976d30f5506370a926a9efb0
aliccce <aliceg@outlook.com>
2016-10-12 13:36:26 +0200

    graphics1

---
+++ graphics1/gloom/gloom/src/program.cpp
@@ -0,0 +1,48 @@
+// Local headers
+#include "program.hpp"
+#include "gloom/gloom.hpp"
+
+
+void runProgram(GLFWwindow* window)
+{
+    // Set GLFW callback mechanism(s)
+    glfwSetKeyCallback(window, keyboardCallback);
+
+    // Enable depth (Z) buffer (accept "closest" fragment)
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    // Configure miscellaneous OpenGL settings
+    glEnable(GL_CULL_FACE);
+
+    // Set default colour after clearing the colour buffer
+    glClearColor(0.3f, 0.3f, 0.4f, 1.0f);
+
+    // Set up your scene here (create Vertex Array Objects, etc.)
+
+    // Rendering Loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Clear colour and depth buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Draw your scene here
+
+        // Handle other events
+        glfwPollEvents();
+
+        // Flip buffers
+        glfwSwapBuffers(window);
+    }
+}
+
+
+void keyboardCallback(GLFWwindow* window, int key, int scancode,
+                      int action, int mods)
+{
+    // Use escape key for terminating the GLFW window
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
